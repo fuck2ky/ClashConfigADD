b96b5cf66150fe38a4459614d3194d164a623699
Washington George <Georgeodin@outlook.com>
2020-04-09 22:48:18 +0800

    更新信息

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Clash策略添加工具")]
-[assembly: AssemblyDescription("Clash策略添加工具")]
+[assembly: AssemblyTitle("")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Odin")]
 [assembly: AssemblyProduct("ClashConfigADD")]
@@ -51,6 +51,6 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.1")]
-[assembly: AssemblyFileVersion("0.0.1.1")]
-[assembly: NeutralResourcesLanguage("zh-Hans")]
+[assembly: AssemblyVersion("0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: NeutralResourcesLanguage("")]
